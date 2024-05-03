b27752a84df97fab33f75529b5565a7d8243f042
AntonioLaurance <74690107+AntonioLaurance@users.noreply.github.com>
2024-05-03 14:12:19 -0600

    Fix the error for revory popular movies with a given API

--- src/pages/Show/Show.tsx
+++ src/pages/Show/Show.tsx
@@ -1,5 +1,7 @@
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+
+import { getDetails } from '../../services/movies/getDetails';
 
 const Show: React.FC = () => {
     const { id } = useParams();
@@ -10,16 +12,16 @@ const Show: React.FC = () => {
     const [loading, setLoading] = useState(true);
 
     const [isFavorite, setIsFavorite] = useState<boolean>(false);
-    const [favorites, setFavorites] = useState<string>("");  // "["381372"]"
+    const [favorites, setFavorites] = useState<string>("");  // "["3982732"]"
 
     const goBack = () => {
         navigate(-1);
     };
 
     const addFavorite = () => {
-        const favs = favorites.length > 0 ? JSON.parse(favorites) : [];  // "["2387238"]" -> 
-        const newFavorites = [...favs, id]; 
-        setFavorites(JSON.stringify(newFavorites));
+        const favs = favorites.length > 0 ? JSON.parse(favorites) : [];  // "["3982732"]" -> ["3982732"]
+        const newFavorites = [...favs, id];  // ["3982732", "9823782"]
+        setFavorites(JSON.stringify(newFavorites));  // "["3982732", "9823782"]"
         setIsFavorite(true);
         localStorage.setItem("favorites", JSON.stringify(newFavorites));
     }
@@ -33,11 +35,18 @@ const Show: React.FC = () => {
         localStorage.setItem("favorites", JSON.stringify(newFavorites));
     }
 
-    /*
     const getMovieDetail = async () => {
-        await getDetails(String(id));
+        await getDetails(String(id))
+            .then((res) => {
+                if(res && res.data){
+                    setShow(res.data);
+                }
+            })
+            .catch((err) => {
+                console.log(err, "err");
+            });
+        setLoading(false);
     }
-    */
 
     useEffect(() => {
         // llamar el endpoint de detalles de una película 
@@ -46,24 +55,36 @@ const Show: React.FC = () => {
         if(favs.includes(String(id))){
             setIsFavorite(true);
         }
+        setLoading(true);
+        getMovieDetail();
     }, []);
 
     return (
         <div>
-            <div>Show id: { id } </div>
-            <div>Título desde el state: {location.state.name}</div>
-            <div>Título desde el servicio:</div>
-            <button onClick={goBack}>Ir atrás</button>
-            {isFavorite ? (
-                <div>
-                    <button className="p4 bg-blue-500">
-                        Remove from favorites
-                    </button>
-                </div>
-            ): (
-                <div>
-                </div>
-            )} 
+            {loading ? (
+                <span>loading...</span>
+            ) : (
+                <>
+                    <div>Show id: { id } </div>
+                    <div>Título desde el state: {location.state.name}</div>
+                    <div>Título desde el servicio: {show.title}</div>
+                    <div>Para adultos desde servicio: {show.adult ? "Yes" : "No"}</div>
+                    <button onClick={goBack}>Ir atrás</button>
+                    {isFavorite ? (
+                        <div>
+                            <button className="p4 bg-blue-500" onClick={removeFavorite}>
+                                Remove from favorites
+                            </button>
+                        </div>
+                    ): (
+                        <div>
+                            <button className="p4 bg-red-500" onClick={addFavorite}>
+                                Add to favorites
+                            </button>
+                        </div>
+                    )}
+                </>
+            )}          
         </div>
     );
 };
--- src/services/movies/getPopularMovies.ts
+++ src/services/movies/getPopularMovies.ts
@@ -1,8 +1,8 @@
-import httpInstance from "../htttpInstance";
+import httpInstance from "../httpInstance";
 
 export const getPopularMovies = async () => {
     let res: any;
-    const endpoint = `popular?api_key=${process.env.REACT_MDB_API_KEY}&language=en-US`;
+    const endpoint = `popular?api_key=${process.env.REACT_APP_MDB_API_KEY}&language=en-US`;
     await httpInstance
         .get(endpoint)
         .then((response) => {
